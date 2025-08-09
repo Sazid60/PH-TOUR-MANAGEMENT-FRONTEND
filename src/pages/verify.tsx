c7e961c07c0e7afaa8427d95eaeeec6bb16cf664
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-08-09 19:14:57 +0600

    37-1 Creating the OTP Input Card Interface

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -1,11 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useLocation, useNavigate } from "react-router";
+import z from "zod";
+
+const FormSchema = z.object({
+  pin: z.string().min(6, {
+    message: "Your one-time password must be 6 characters.",
+  }),
+})
 
 export default function Verify() {
   const location = useLocation();
   const navigate = useNavigate();
   const [email] = useState(location.state);
 
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      pin: "",
+    },
+  })
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+
+    console.log(data)
+
+  }
+
+  //! Needed but for now its commented 
   useEffect(() => {
     if (!email) {
       navigate("/");
@@ -13,8 +41,57 @@ export default function Verify() {
   }, [email]);
 
   return (
-    <div>
-      <h1> This is Verify component </h1>
+    <div className="grid place-content-center h-screen">
+      <Card className="max-w-fit">
+        <CardHeader>
+          <CardTitle className="text-xl">
+            Verify Your Email Address !
+          </CardTitle>
+          <CardDescription>
+            Please Enter Your 6-digit code we sent to <br /> {email}
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Form {...form}>
+            <form id="otp-form" onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+              <FormField
+                control={form.control}
+                name="pin"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>One-Time Password</FormLabel>
+                    <FormControl>
+                      <InputOTP maxLength={6} {...field}>
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                          <InputOTPSlot index={1} />
+                          <InputOTPSlot index={2} />
+                          <InputOTPSlot index={3} />
+                          <InputOTPSlot index={4} />
+                          <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+                    <FormDescription>
+                      Please enter the one-time password sent to your phone.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* <Button type="submit">Submit</Button> */}
+            </form>
+          </Form>
+        </CardContent>
+
+        <CardFooter className="flex justify-end">
+          <Button form ="otp-form" type="submit">
+            Submit
+          </Button>
+        </CardFooter>
+
+      </Card>
     </div>
   );
 }
